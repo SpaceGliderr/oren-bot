80ae37dec90ee00d9cf0f7073b5fb2d8bd6e9eec
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2021-08-14 18:02:29 +0800

    Generated prisma migrations

---
+++ src/database/migrations/20210813135234_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "username" TEXT NOT NULL,
+    "discriminator" TEXT NOT NULL,
+    "defaultAvatarURL" TEXT,
+
+    PRIMARY KEY ("id")
+);
