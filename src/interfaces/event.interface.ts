46e53d6acc72dcfa730d1577c915b3c435d17491
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2021-08-22 18:18:55 +0800

    Fix once bug in event interface

--- src/interfaces/event.interface.ts
+++ src/interfaces/event.interface.ts
@@ -2,6 +2,6 @@ import { SuperClient as Client } from '../client';
 
 export interface IEvent {
   name: string;
-  once?: boolean;
+  once: boolean;
   execute: (client: Client, ...args: any[]) => Promise<void>;
 }
