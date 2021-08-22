b762b74c6562e3020c6aef4547684b733d5368d6
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2021-08-22 18:17:45 +0800

    Add database connection to messageCreate event

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,10 +1,59 @@
+import { Prisma } from '@prisma/client';
 import { Message } from 'discord.js';
 import { IEvent } from '../interfaces/event.interface';
 
-export const onMessage: IEvent = {
+export const messageCreate: IEvent = {
   name: 'messageCreate',
+  once: false,
   execute: async (client, message: Message) => {
     if (message.author.bot) return;
+
+    const { author, guild } = message;
+
+    let userCreate: Prisma.UserCreateInput = {
+      id: author.id,
+      username: author.username,
+      discriminator: author.discriminator,
+      defaultAvatarURL: author.avatar,
+    };
+
+    const userUpdate: Prisma.UserUpdateInput = {
+      username: author.username,
+      discriminator: author.discriminator,
+      defaultAvatarURL: author.avatar,
+    };
+
+    if (guild) {
+      await client.prisma.guild.upsert({
+        where: {
+          id: guild.id,
+        },
+        update: {},
+        create: {
+          id: guild.id,
+        },
+      });
+
+      userCreate.guildMembers = {
+        create: [
+          {
+            guild: {
+              connect: {
+                id: guild?.id,
+              },
+            },
+          },
+        ],
+      };
+    }
+
+    await client.prisma.user.upsert({
+      where: {
+        id: author.id,
+      },
+      update: userUpdate,
+      create: userCreate,
+    });
 
     const prefix = '.';
     if (!message.content.startsWith(prefix)) return;
@@ -20,4 +69,4 @@ export const onMessage: IEvent = {
   },
 };
 
-export default onMessage;
+export default messageCreate;
